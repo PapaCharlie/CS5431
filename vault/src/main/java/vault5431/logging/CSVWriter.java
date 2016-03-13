8b3a0b23c4e018d5e6912ec1036be0942b0950cb
Paul Chesnais <paul.chesnais@gmail.com>
2016-03-13 14:53:18 -0400

    Password class serializer

--- vault/src/main/java/vault5431/logging/CSVWriter.java
+++ vault/src/main/java/vault5431/logging/CSVWriter.java
@@ -1,9 +1,5 @@
 package vault5431.logging;
 
-import static vault5431.User.getLogFile;
-import static vault5431.User.getPublicEncryptionKeyFile;
-import vault5431.crypto.Base64String;
-
 /**
  * Created by papacharlie on 3/11/16.
  */
