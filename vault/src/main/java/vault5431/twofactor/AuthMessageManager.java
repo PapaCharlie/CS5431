eac7b59b91f1bf451059580a82e8055129d83387
Paul Chesnais <paul.chesnais@gmail.com>
2016-04-13 18:15:17 -0400

    Fixed password IDs

--- vault/src/main/java/vault5431/twofactor/AuthMessageManager.java
+++ vault/src/main/java/vault5431/twofactor/AuthMessageManager.java
@@ -1,5 +1,7 @@
 package vault5431.twofactor;
 
+import vault5431.users.User;
+
 import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -12,15 +14,15 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
  * Created by cyj on 4/9/16.
  */
 public class AuthMessageManager {
-    protected static ConcurrentHashMap<String, AuthMessage> authCodeManager = new ConcurrentHashMap<String, AuthMessage>();
+    private static final ConcurrentHashMap<String, AuthMessage> authCodeManager = new ConcurrentHashMap<String, AuthMessage>();
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private static final int TIME_TO_EXPIRE = 60000;
 
 
-    protected static void addToManager(String username, AuthMessage m) {
-        authCodeManager.put(username, m);
+    protected static void addToManager(User user, AuthMessage m) {
+        authCodeManager.put(user.hash.toString(), m);
         Runnable removeCode = () -> {
-                authCodeManager.remove(username);
+                authCodeManager.remove(user.hash.toString());
                 System.out.println("test runnable: " + authCodeManager.size());
         };
 //        System.out.println("in addToManager function: " + authCodeManager.size());
@@ -28,4 +30,14 @@ public class AuthMessageManager {
                 removeCode, TIME_TO_EXPIRE, MILLISECONDS);
 //        System.out.println("After scheduler set: " + authCodeManager.size());
     }
+
+    public static boolean verifyAuthMessage(User user, int code) {
+        if (authCodeManager.contains(user.hash.toString())) {
+            AuthMessage message = authCodeManager.get(user.hash.toString());
+            return message.authCode == code;
+        } else {
+            return false;
+        }
+    }
+
 }
